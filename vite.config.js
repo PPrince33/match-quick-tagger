bf4b046ec60bd1e0a411c9f25c7c5892055da40e
PPrince33 <137063047+PPrince33@users.noreply.github.com>
2026-02-26 18:02:49 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // [https://vitejs.dev/config/](https://vitejs.dev/config/)
 export default defineConfig({
   plugins: [react()],
-  base: '/match-quick-tagger/', // MUST MATCH YOUR EXACT REPOSITORY NAME
+  base: './', // MUST MATCH YOUR EXACT REPOSITORY NAME
 })
