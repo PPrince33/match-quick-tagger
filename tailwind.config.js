7415e0c589d018b149aacd44a44ca7d596c2dca9
PPrince33 <137063047+PPrince33@users.noreply.github.com>
2026-02-26 18:56:11 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,16 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { extend: {} },
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        // This overrides the default font to use monospace everywhere
+        sans: ['"Courier New"', 'Courier', 'monospace'], 
+      },
+    },
+  },
   plugins: [],
 }
